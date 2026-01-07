cef15671e8f84ac29c871f6924e6f80fda3515d5
sicsee <nicolasdsgodinho@gmail.com>
2026-01-07 18:24:06 -0300

    feat: setup tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,13 @@
-
-<h1>
-    Hello World
-</h1>
-
-<p>
-    Olá, {{ $nome }}
-</p>
-
-<p>Seus hábitos são:</p>
-
-@foreach ($habitos as $item)
-    <li>
-        {{ $item }}
-    </li>
-@endforeach
-
-<p>Sua idade é: {{ $idade }}</p>
-
-@auth
-    <p>Você está autenticado</p>
-@endauth
-
-@guest
-   <p>Você não está autenticado</p>
-@endguest
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>{{ config('app.name') }}</title>
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline text-green-500">Olá Mundo</h1>
+</body>
+</html>
